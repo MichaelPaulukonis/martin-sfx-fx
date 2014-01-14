a99aa249532a8a7d8dd9cb8ec78ed87888ec26ab
Michael Paulukonis <xraysmalevich@gmail.com>
2014-01-14 13:08:16 -0500

    a man, a plan, dependencies, martin.js, a canal. Panama!

---
+++ martin.js
@@ -0,0 +1,4 @@
+/* TODO select a random soound-effect from don-martin list
+ https://raw.github.com/cjwinchester/cjwinchester.github.io/master/sandbox/martin/martin.js
+ google-search for an image - https://github.com/dariusk/node-google-images
+*/
