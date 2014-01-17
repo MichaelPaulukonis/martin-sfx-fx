1e65b42890f116d07c95d90192f57bcf1d0e516a
Michael Paulukonis <xraysmalevich@gmail.com>
2014-01-16 22:14:05 -0500

    discarded node concept and went old-school client-side. have a working prototype.

--- javascript/sound.js
+++ javascript/sound.js
@@ -1,1 +1,44 @@
-$('#description')[0].innerHTML = "hello world!";
+/**
+
+ select a random soound-effect from don-martin list
+ https://raw.github.com/cjwinchester/cjwinchester.github.io/master/sandbox/martin/martin.js
+ google-search for an image
+ show it plus the sound effect
+ PROFIT!!!
+
+ **/
+
+google.load('search', '1');
+
+var imageSearch, mart;
+
+
+var searchComplete = function() {
+
+    if (imageSearch.results && imageSearch.results.length > 0) {
+
+        // alert(imageSearch.results.length);
+        var url = (_.sample(imageSearch.results)).url;
+        var img = $('#soundimage');
+        img.attr('src', url);
+
+        img.load(function() {
+            $(this).fadeIn();
+            $('#description')[0].innerHTML = mart.sound;
+        });
+
+    }
+
+};
+
+var onLoad = function() {
+
+    imageSearch = new google.search.ImageSearch();
+    imageSearch.setSearchCompleteCallback(this, searchComplete, null);
+
+    // can't execute imageSearch until onLoad has completed.
+    mart = _.sample(don_martin);
+    imageSearch.execute(mart.description);
+};
+
+google.setOnLoadCallback(onLoad);
